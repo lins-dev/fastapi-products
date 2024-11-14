084171e137696af1ceab6c878aaae2529abfe1f5
Lucas Lins <lucas.fsl.92@gmail.com>
2024-11-13 23:13:14 -0300

    feature: remove unused imports in main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import Depends, FastAPI
 
-from .dependencies import get_query_token, get_token_header
-from .internal import admin
-from .routers import items, users
 
 app = FastAPI()
 
