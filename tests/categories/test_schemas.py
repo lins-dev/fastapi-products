32c70a18d84e89b9e360cae8a70ddaa47989c324
Lucas Lins <lucas.fsl.92@gmail.com>
2024-11-27 23:44:05 -0300

    feature: adding first test file and configuration in pyproject.toml

---
+++ tests/categories/test_schemas.py
@@ -0,0 +1,35 @@
+import pytest
+from app.categories.schemas import Category
+
+def test_category_schema():
+    result = Category(
+        name = 'Roupa',
+        slug = 'roupa'
+    )
+
+    expected = {
+        'name' : 'Roupa',
+        'slug' : 'roupa'
+    }
+    print(f"Result: {result.model_dump()}, Expected: {expected}")
+    assert result.model_dump() == {
+        'name' : 'Roupa',
+        'slug' : 'roupa'
+    }
+
+def test_category_schema_invalid_slug():
+    with pytest.raises(ValueError):
+        category = Category(
+            name = 'Roupa',
+            slug = 'roupa error'
+        ),
+    with pytest.raises(ValueError):
+        category = Category(
+            name = 'Roupa',
+            slug = 'çça error'
+        ),
+    with pytest.raises(ValueError):
+        category = Category(
+            name = 'Roupa',
+            slug = 'Roupa error'
+        )
